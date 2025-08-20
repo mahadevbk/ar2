3e810ed013a4794dc6bcea66ee12668ebc63b941
mahadevbk <68096457+mahadevbk@users.noreply.github.com>
2025-08-20 19:26:46 +0400

    Update ar.py

--- ar.py
+++ ar.py
@@ -1,10 +1,10 @@
 # dev's scratch pad
-# tab[0] 1455
-#tab [1] 1912
-#tab [2] 2155
-#tab [3] 2244
-#tab [4] 2308
-#tab [5] 2684
+# tab[0] 1602
+#tab [1] 1959
+#tab [2] 2355
+#tab [3] 2444
+#tab [4] 2508
+#tab [5] 2970
 # court names 1367
 # profile image line 55
 #
