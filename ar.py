36520bfd9e0a38224a7faf585a9856f0f3724446
mahadevbk <68096457+mahadevbk@users.noreply.github.com>
2025-08-23 03:27:08 +0400

    Update ar.py

--- ar.py
+++ ar.py
@@ -884,8 +884,8 @@ def display_player_insights(selected_players, players_df, matches_df, rank_df, p
 
         with col1:  # Left column for visuals
             if profile_image:
-                #st.image(profile_image, width=150)
-                st.markdown(f'<img src="{profile_image}" class="profile-image" style="width: 175px; height: 175px;">', unsafe_allow_html=True)
+                st.image(profile_image, width=150)
+                #st.markdown(f'<img src="{profile_image}" class="profile-image" style="width: 175px; height: 175px;">', unsafe_allow_html=True)
 
             st.markdown("##### Win/Loss")
             win_loss_chart = create_win_loss_donut(wins, losses)
