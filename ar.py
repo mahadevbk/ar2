fb83703c575634e4b9a6e3dae3bfc4244905c0f5
mahadevbk <68096457+mahadevbk@users.noreply.github.com>
2025-08-28 10:09:07 +0400

    Update ar.py

--- ar.py
+++ ar.py
@@ -1735,7 +1735,7 @@ court_names = [
 fun_verbs = [
     "defeated", "thrashed", "crushed", "beat the hell out of",
     "smashed", "obliterated", "demolished", "outplayed",
-    "vanquished", "dominated", "trounced", "routed", "get the better of",  "inexplicably, def.", "in an upset, def."
+    "vanquished", "dominated", "trounced", "routed", "got the better of",  "inexplicably, def.", "in an upset, def."
 ]
 
 
