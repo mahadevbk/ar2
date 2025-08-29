1fb0ca2c13263e89f2fe9d8cf89126c70cfbe0c1
mahadevbk <68096457+mahadevbk@users.noreply.github.com>
2025-08-29 17:48:23 +0400

    Update ar.py

--- ar.py
+++ ar.py
@@ -1692,7 +1692,7 @@ def get_match_verb_and_gda(row):
     Returns a tuple of (verb, gda).
     """
     match_0to1 = ["squeaked past", "barely beat", "got lucky against", "got lucky against","survived (barely)" ]
-    match_1to2_9 = ["with luck & gusto, def."," defeated","edged past"]
+    match_1to2_9 = ["with luck & gusto, def."," defeated","edged past","squeaked past", "barely beat", "got lucky against", "got lucky against","survived (barely)" ]
     match_3to4_9 = ["outplayed", "dominated", "got the better of", "vanquished", "trounced"]
     match_5to6 = ["thrashed", "crushed", "beat the hell out of", "smashed", "obliterated", 
                         "demolished", "routed", "destroyed","deadpooled"]
