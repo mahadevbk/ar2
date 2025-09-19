4bcbffa78f3b06db4b4070a33306465071688506
mahadevbk <68096457+mahadevbk@users.noreply.github.com>
2025-09-19 19:28:50 +0400

    Update ar.py

--- ar.py
+++ ar.py
@@ -2502,9 +2502,9 @@ with tabs[0]:
             # --- UPDATED: Detailed Stats Display with Match Counts ---
             st.markdown(f"""
             <div style="line-height: 2;">
-                <span class="games-won-col" style="display: block;">Games Won: {int(player_data['Games Won'])}</span>
-                <span class="game-diff-avg-col" style="display: block;">Game Diff Avg: {player_data['Game Diff Avg']:.2f}</span>
-                <span class="cumulative-game-diff-col" style="display: block;">Cumulative Game Diff: {int(player_data['Cumulative Game Diff'])}</span>
+                <span class="games-won-col" style="display: block;"> {int(player_data['Games Won'])}</span>
+                <span class="game-diff-avg-col" style="display: block;"> {player_data['Game Diff Avg']:.2f}</span>
+                <span class="cumulative-game-diff-col" style="display: block;"> {int(player_data['Cumulative Game Diff'])}</span>
                 <span class="performance-score-col" style="display: block;">
                     <span style='font-weight:bold; color:#bbbbbb;'>Performance Score: </span>
                     <span style='font-weight:bold; color:#fff500;'>Doubles: {doubles_perf_score:.1f} ({int(player_data["Doubles Matches"])}), Singles: {singles_perf_score:.1f} ({int(player_data["Singles Matches"])})</span>
@@ -2874,6 +2874,20 @@ with tabs[0]:
         else:
             for index, row in rank_df.iterrows():
                 display_ranking_card(row, players_df, filtered_matches, partner_stats, rank_df_doubles, rank_df_singles, key_prefix=f"combined_{index}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
