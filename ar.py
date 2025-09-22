7aef276f596a36dd055d8e0769599772d40c86f0
mahadevbk <68096457+mahadevbk@users.noreply.github.com>
2025-09-22 15:18:14 +0400

    Update ar.py

--- ar.py
+++ ar.py
@@ -2307,7 +2307,7 @@ def generate_match_card(row, image_url):
     # --- MODIFICATION: Set text and shadow colors ---
     white_fill = (255, 255, 255, 255)
     shadow_fill = (204, 255, 0, 50) # Semi-transparent optic yellow
-    shadow_offset = 2
+    shadow_offset = 4
     
     for text, y in zip([players_text, set_text, gda_text], y_positions):
         # Draw shadow
